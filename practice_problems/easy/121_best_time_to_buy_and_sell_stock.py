13e82ac24bd5b56dc61998e816274bdc3f997b73
vikaspalakurthi <vikaspalakurthi@users.noreply.github.com>
2025-08-03 05:24:54 +0000

    feat: added another easy problem

---
+++ practice_problems/easy/121_best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = prices[0]
+        profit = 0
+
+        for price in prices:
+            if buy > price: 
+                buy = price
+            
+            profit = max(profit, price - buy)
+        
+        return profit
