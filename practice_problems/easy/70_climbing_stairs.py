3f4db5effd7adc0f78639c8b6ab3dd36d7b13041
vikaspalakurthi <vikaspalakurthi@users.noreply.github.com>
2025-08-03 05:46:19 +0000

    feat: added 70. leet code problem

---
+++ practice_problems/easy/70_climbing_stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <=3:
+            return n
+        
+        prev1 = 3
+        prev2 = 2
+        cur = 0
+
+        for _ in range(3, n):
+            prev2, prev1 = prev1, prev1 + prev2
+        
+        return prev1
